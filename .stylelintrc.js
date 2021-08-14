7c1b3d34fd99cc532d68187d5b61686aeb7a3fb1
Senja Jarva <senja.jarva@futurice.com>
2021-08-14 16:23:58 +0300

    Arrange stylelint rules according to their purpose

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,10 +1,13 @@
 module.exports = {
     extends: 'stylelint-config-standard',
     rules: {
+        // Rules enforcing writing style
         indentation: 4,
+        // Rules preventing errors
         'function-calc-no-invalid': true,
+        'unit-no-unknown': true,
+        // Rules improving accessibility
         'unit-allowed-list': ['rem', 'deg', '%', 's'],
         'unit-disallowed-list': ['px', 'em'],
-        'unit-no-unknown': true,
     },
 }
