031a49eaf323ee32c7fcc8868220153ea93ede5f
Senja Jarva <senja.jarva@futurice.com>
2021-08-14 16:35:25 +0300

    Add a11 stylelint plugin and some rules from it

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,11 +1,14 @@
 module.exports = {
-    extends: 'stylelint-config-standard',
+    extends: ['stylelint-config-standard', 'stylelint-a11y/recommended'],
+    plugins: ['stylelint-a11y'],
     rules: {
         // Rules enforcing writing style
         indentation: 4,
         // Rules preventing errors
         'function-calc-no-invalid': true,
         'unit-no-unknown': true,
+        'a11y/no-obsolete-attribute': true,
+        'a11y/no-obsolete-element': true,
         // Rules improving accessibility
         'unit-allowed-list': ['rem', 'deg', '%', 's'],
         'unit-disallowed-list': ['px', 'em'],
