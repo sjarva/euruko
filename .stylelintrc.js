14a8897cf4ea38feb6010b5a2dc18b960f923319
Senja Jarva <senja.jarva@futurice.com>
2021-08-14 16:15:41 +0300

    Add stylelint rules concerning units
    Allow rem, deg, percent and seconds as units. Ban pixels and ems. Ban also unknown units. Fix all the files that are breaking these rules.

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -3,5 +3,8 @@ module.exports = {
     rules: {
         indentation: 4,
         'function-calc-no-invalid': true,
+        'unit-allowed-list': ['rem', 'deg', '%', 's'],
+        'unit-disallowed-list': ['px', 'em'],
+        'unit-no-unknown': true,
     },
 }
